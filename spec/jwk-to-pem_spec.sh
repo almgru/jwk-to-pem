50005c806aa4f6ecc51c5974d641d184505126a1
Daniel Alm Grundstrom <daniel+sourcehut@almgru.com>
2020-01-05 07:46:44 +0100

    Adds tests for to_hex

--- spec/jwk-to-pem_spec.sh
+++ spec/jwk-to-pem_spec.sh
@@ -3,6 +3,28 @@
 Describe 'jwk-to-pem'
 	Include ./bin/jwk-to-pem
 
+	Describe 'to_hex()'
+	    It 'converts raw data to hexadecimal'
+		When call to_hex "Oc9j"
+		The output should equal '0x4F63396A'
+	    End
+
+	    It 'uses 0x as a prefix'
+		When call to_hex 'F&'
+		The output should start with '0x'
+	    End
+
+	    It 'produces uppercase letters'
+		When call to_hex 'ͺ' 
+		The output should equal '0xCDBA'
+	    End
+
+	    It 'produces no output with no argument'
+		When call to_hex
+		The output should equal ''
+	    End
+	End
+
 	Describe 'base64url_decode()'
 		# base64_pad stub that just echoes its arg
 		base64_pad() {
