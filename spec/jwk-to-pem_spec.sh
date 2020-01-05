01bc0f7c3a012702f3fd46ee33f04c0d537b6dab
Daniel Alm Grundström <almgru+github@fastmail.com>
2020-01-05 15:12:47 +0100

    indentation fix

--- spec/jwk-to-pem_spec.sh
+++ spec/jwk-to-pem_spec.sh
@@ -4,25 +4,25 @@ Describe 'jwk-to-pem'
 	Include ./bin/jwk-to-pem
 
 	Describe 'to_hex()'
-	    It 'converts raw data to hexadecimal'
-		When call to_hex "Oc9j"
-		The output should equal '0x4F63396A'
-	    End
+		It 'converts raw data to hexadecimal'
+			When call to_hex "Oc9j"
+			The output should equal '0x4F63396A'
+		End
 
-	    It 'uses 0x as a prefix'
-		When call to_hex 'F&'
-		The output should start with '0x'
-	    End
+		It 'uses 0x as a prefix'
+			When call to_hex 'F&'
+			The output should start with '0x'
+		End
 
-	    It 'produces uppercase letters'
-		When call to_hex 'ͺ' 
-		The output should equal '0xCDBA'
-	    End
+		It 'produces uppercase letters'
+			When call to_hex 'ͺ' 
+			The output should equal '0xCDBA'
+		End
 
-	    It 'produces no output when called with no argument'
-		When call to_hex
-		The output should equal ''
-	    End
+		It 'produces no output when called with no argument'
+			When call to_hex
+			The output should equal ''
+		End
 	End
 
 	Describe 'base64url_decode()'
