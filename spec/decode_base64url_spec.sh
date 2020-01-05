b15a9a6b8b29d699a097f712eb9a7a6a0a91b15d
Daniel Alm Grundstrom <daniel+sourcehut@almgru.com>
2020-01-06 00:36:16 +0100

    Update decode_base64url tests

--- spec/decode_base64url_spec.sh
+++ spec/decode_base64url_spec.sh
@@ -3,23 +3,21 @@
 Describe 'base64url_decode()'
 	Include ./bin/jwk-to-pem
 
-	# stub
-	base64_pad() {
-		printf "%s" "$1"
-	}
-
 	It 'decodes base64url encoded data'
-		When call base64url_decode 'aGVsbG8='
+		Data "aGVsbG8="
+		When call decode_base64url
 		The output should equal 'hello'
 	End
 
 	It 'correctly decodes data with underscores'
-		When call base64url_decode 'b2s_'
+		Data "b2s_"
+		When call decode_base64url
 		The output should equal 'ok?'
 	End
 
 	It 'correctly decodes data with dashes'
-		When call base64url_decode 'aGl-'
+		Data "aGl-"
+		When call decode_base64url
 		The output should equal 'hi~'
 	End
 End
