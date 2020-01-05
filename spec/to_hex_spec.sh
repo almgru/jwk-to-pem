2b87bf52cf98843e2e5d2e761405bac5ef67d44a
Daniel Alm Grundstrom <daniel+sourcehut@almgru.com>
2020-01-06 00:38:36 +0100

    Update to_hex tests

--- spec/to_hex_spec.sh
+++ spec/to_hex_spec.sh
@@ -3,23 +3,15 @@
 Describe 'to_hex()'
 	Include ./bin/jwk-to-pem
 
-	It 'converts raw data to hexadecimal'
-		When call to_hex "Oc9j"
-		The output should equal '0x4F63396A'
-	End
-
-	It 'uses 0x as a prefix'
-		When call to_hex 'F&'
-		The output should start with '0x'
+	It 'converts data to hexadecimal'
+		Data "Oc9j"
+		When call to_hex
+		The output should start with '4F63396A'
 	End
 
 	It 'produces uppercase letters'
-		When call to_hex 'ͺ' 
-		The output should equal '0xCDBA'
-	End
-
-	It 'produces no output when called with no argument'
+		Data "ͺ"
 		When call to_hex
-		The output should equal ''
+		The output should start with 'CDBA'
 	End
 End
