8bfd01840a238323611585c1d0fe78ad9b74ed44
Daniel Alm Grundström <almgru+github@fastmail.com>
2020-01-05 15:56:49 +0100

    Adds json_to_hex test

--- spec/jwk-to-pem_spec.sh
+++ spec/jwk-to-pem_spec.sh
@@ -3,6 +3,21 @@
 Describe 'jwk-to-pem'
 	Include ./bin/jwk-to-pem
 
+	Describe 'json_to_hex()'
+		# shellcheck disable=SC2034
+		JWK_FILE=/dev/stdin
+
+		It 'takes a json key and converts its value to hex'
+			base64url_decode() { printf 10; }
+			to_hex() { printf '0x0A'; }
+
+			Data '{"a":10,"b":20}'
+
+			When call json_to_hex "a"
+			The output should equal '0x0A'
+		End
+	End
+
 	Describe 'to_hex()'
 		It 'converts raw data to hexadecimal'
 			When call to_hex "Oc9j"
